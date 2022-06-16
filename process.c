9aa90fda52b15a4d8b92630066d2390f36b08341
Julius M. Kanani <juliusm.kanani@gmail.com>
2022-06-16 22:27:39 +0300

    parses a given token to obtain an opcode

---
+++ process.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+/**
+ * process_line - process and parse line
+ * @stack: the stack
+ *
+ * Return: integer
+ */
+int process_line(stack_t **stack)
+{
+	instruction_t inst[] = {
+		{"push", add_dnodeint},
+		{"pall", print_dlistint},
+		{"pint", print_top},
+		{"pop", pop_top},
+		{"swap", swap_top},
+		{"nop", nothing},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while ((inst + i)->opcode)
+	{
+		if (strcmp((inst + i)->opcode, *data.args) == 0)
+		{
+			(inst + i)->f(stack, data.line_number);
+			return (0);
+		}
+		i++;
+	}
+	push_error(15);
+	return (1);
+}
